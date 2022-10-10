9ac5003a956a02c5520a77684197b1613934727e
Olena Korenieva <olena.korenieva@gmail.com>
2022-10-10 14:25:13 +0200

    add dependencies

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const getEl = selector => document.querySelector(selector);
+
+const galleryWrapper = getEl('.gallery');
+
+const galleryArr = galleryItems.map(
+  ({
+    original,
+    preview,
+    description,
+  }) => `<a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}">
+     </a>`
+);
+
+galleryWrapper.insertAdjacentHTML('beforeend', galleryArr.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+});
