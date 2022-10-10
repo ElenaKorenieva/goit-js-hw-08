f1de07767d7bf52ffe842523992def9ec6bb7eff
Olena Korenieva <olena.korenieva@gmail.com>
2022-10-10 14:35:55 +0200

    Update 01-gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,11 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// Change code below this line
 
 const getEl = selector => document.querySelector(selector);
 
